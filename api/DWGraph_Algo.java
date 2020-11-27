cd011b8c3fad9fe6e2d2cb1e66c0be0389119350
amiel lejzor <amiel349@gmail.com>
2020-11-27 13:26:21 +0200

    new class NodeData and some implements in DWG_algo

--- api/DWGraph_Algo.java
+++ api/DWGraph_Algo.java
@@ -1,22 +1,37 @@
 package api;
 
+import java.util.Collection;
 import java.util.List;
 
 public class DWGraph_Algo implements dw_graph_algorithms {
+    private directed_weighted_graph _graph;
 
     @Override
     public void init(directed_weighted_graph g) {
-
+     _graph=g;
     }
 
     @Override
     public directed_weighted_graph getGraph() {
-        return null;
+        return _graph;
     }
 
     @Override
-    public directed_weighted_graph copy() {
-        return null;
+    public directed_weighted_graph copy(){
+    directed_weighted_graph copy=new DWGraph_DS();
+        Collection<node_data>nodes=_graph.getV();
+        for (node_data node : nodes) {
+            node_data copyNode=new NodeData(node.getKey());
+            copyNode.setLocation(node.getLocation());
+            copy.addNode(copyNode);
+        }
+        for (node_data node : nodes) {
+            Collection<edge_data>neiNodes=_graph.getE(node.getKey());
+            for (edge_data neiNode : neiNodes) {
+             copy.connect(neiNode.getSrc(),neiNode.getDest(), neiNode.getWeight());
+            }
+        }
+        return copy;
     }
 
     @Override
