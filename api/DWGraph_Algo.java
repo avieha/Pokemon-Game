30eaa06dde61ca18aa8bd6cb395073dc0871eabd
amiel lejzor <amiel349@gmail.com>
2020-11-29 00:37:38 +0200

    DWG_algo- isconnected DWGraph - tostring

--- api/DWGraph_Algo.java
+++ api/DWGraph_Algo.java
@@ -1,7 +1,9 @@
 package api;
 
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DWGraph_Algo implements dw_graph_algorithms {
     private directed_weighted_graph _graph;
@@ -36,7 +38,39 @@ public class DWGraph_Algo implements dw_graph_algorithms {
 
     @Override
     public boolean isConnected() {
-        return false;
+        if(this._graph==null)
+            return true;
+        if(this._graph.nodeSize()==0)
+            return true;
+
+        Collection<node_data>t= this._graph.getV();
+        for (node_data recentNode : t) {
+            Collection<node_data>secondt= this._graph.getV();
+            for (node_data x : secondt) {
+                this._graph.getNode(x.getKey()).setTag(0);
+            }
+
+            Queue<node_data> q=new LinkedList<node_data>();
+            q.add(recentNode);
+            q.peek().setTag(1);
+            while (!q.isEmpty()) {
+                Collection<edge_data>edges = this._graph.getE(q.peek().getKey());
+                q.remove();
+                for (edge_data edge : edges) {
+                    if (_graph.getNode(edge.getDest()).getTag() == 0) {
+                        _graph.getNode(edge.getDest()).setTag(1);
+                        q.add(_graph.getNode(edge.getDest()));
+                    }
+
+                }
+            }
+            Collection<node_data> y = this._graph.getV();
+            for (node_data node2 : y) {
+                if (node2.getTag() == 0)
+                    return false;
+            }
+        }
+        return true;
     }
 
     @Override
